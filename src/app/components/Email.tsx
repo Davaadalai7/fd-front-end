a477318b5ba05dfd4616b7f010371b9828067425
Davaadalai7 <skiple7@gmail.com>
2025-02-24 18:16:50 +0800

    test

--- src/app/components/Email.tsx
+++ src/app/components/Email.tsx
@@ -1,34 +1,28 @@
 'use client';
 
 import React from 'react';
-// import { useRouter } from 'next/router';
+import { EmailInput } from './ui/Email-Input';
+import { Input } from '@/components/ui/input';
 
 const SignupComponent = () => {
-//   const router = useRouter();
-
-//   const handleBackClick = () => {
-//     router.back();  // Navigates back to the previous page
-  };
-
   return (
     <div className="flex h-screen">
       {/* Left Side */}
-      <div className="flex flex-col justify-start p-8 w-1/2">
-        <button 
-          onClick={handleBackClick} 
-          className="text-blue-500 mb-4"
-        >
-          &#8592; Back
-        </button>
+      <div className="flex flex-col justify-center p-8 w-1/2">
         <h1 className="text-3xl font-bold mb-4">Create your account</h1>
         <p className="text-lg text-gray-600">Sign up to explore your favorite dishes.</p>
+        <Input/>
       </div>
 
       {/* Right Side */}
-      <div className="w-1/2 bg-cover bg-center" 
-           style={{ backgroundImage: `url('https://res.cloudinary.com/du9etdqhq/image/upload/v1739964999/Frame_1321316047_qcw19i.png')` }}></div>
+      <div
+        className="w-1/2 bg-cover bg-center rounded-xl"
+        style={{
+          backgroundImage: `url('https://res.cloudinary.com/du9etdqhq/image/upload/v1740387500/food-delivery/ulfoy8dmy50dw5iqktaz.png')`,
+        }}
+      ></div>
     </div>
   );
-// };
+};
 
 export default SignupComponent;
